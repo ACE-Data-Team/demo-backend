95000039d95527956c0aee07f23f304625171f01
Prechy <preciousrobinsonokafor@gmail.com>
2025-05-02 11:51:24 +0100

    Added new requirementds and refactored db and queries functions

--- app/db/database.py
+++ app/db/database.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv # type: ignore
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 if DATABASE_URL is None:
     raise ValueError("The DATABASE_URL environment variable is not set. Please set it to proceed.")
 
