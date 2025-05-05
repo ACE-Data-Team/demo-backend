a32f12f93e1e9518ad841eb4f87d3d9fe39c0041
Prechy <preciousrobinsonokafor@gmail.com>
2025-05-05 11:33:09 +0100

    refactored main.py to fetch data from db

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ from app.charts.home_charts import (
     generate_student_trend_chart
 )
 import pandas as pd # type: ignore
-from db import database
+from app.db.database import (
+    get_student_dataframe,
+    get_staff_dataframe
+)
 
 app = FastAPI()
 
@@ -23,41 +26,39 @@ app.add_middleware(
 
 # Load data
 
-async def get_staff_data():
-    return await database.get_staff_dataframe()
 
-async def get_student_data():
-    return await database.get_student_dataframe()
+
+
 
 
 @app.get("/charts/staff-donut", response_class=HTMLResponse)
 async def staff_donut_chart(session: str = "2023/2024"):
+    df_staff = await get_staff_dataframe()
     # Generate the chart HTML
-    df_staff = await get_staff_data()
     chart_html = generate_staff_donut_chart(df_staff, session)
     # Return only the raw chart HTML
     return chart_html
 
 @app.get("/charts/student-donut", response_class=HTMLResponse)
 async def student_donut_chart(session: str = "2023/2024"):
+    df_students = await get_student_dataframe()
     # Generate the chart HTML
-    df_students = await get_student_data()
     chart_html = generate_student_donut_chart(df_students, session)
     # Return only the raw chart HTML
     return chart_html
 
 @app.get("/charts/staff-trend", response_class=HTMLResponse)
 async def staff_trend_chart():
+    df_staff = await get_staff_dataframe()
     # Generate the chart HTML
-    df_staff = await get_staff_data()
     chart_html = generate_staff_trend_chart(df_staff)
     # Return only the raw chart HTML
     return chart_html
 
 @app.get("/charts/student-trend", response_class=HTMLResponse)
 async def student_trend_chart():
+    df_students = await get_student_dataframe()
     # Generate the chart HTML
-    df_students = await get_student_data()
     chart_html = generate_student_trend_chart(df_students)
     # Return only the raw chart HTML
     return chart_html
